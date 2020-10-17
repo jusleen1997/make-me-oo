4641ea09a6fbf534b0be7851c70b201af560d5ce
jusleen kaur rekhi <jusleenkaurrekhi@jusleens-MacBook-Pro.local>
2020-10-17 22:22:24 +0530

    oops design change

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,16 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double distanceFromPoint(Point to) {
+        return Math.sqrt(Math.pow(to.x - this.x, 2) + Math.pow(to.y - this.y, 2));
+    }
+
+    public double angleFromPoint(Point to) {
+        return Math.atan2(to.y - this.y, to.x - this.x);
     }
 }
